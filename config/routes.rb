d92c376715208afeaaae8f3e55bbd642b009262b
tiagreaney <tiagreaney@gmail.com>
2023-08-01 20:52:26 +0100

    Feature: Login, sign up and sign out added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'entries#index'
+  root to: 'main#index'
   resources :entries, only: [:create, :new, :destroy]
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 end
